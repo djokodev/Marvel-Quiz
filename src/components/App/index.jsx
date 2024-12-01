555788c203fa5ee3906b88fbfd8f2b146930a857
M@teo22 <djokodev75@gmail.com>
2024-12-01 01:32:28 +0100

    create folder and implement App, Header, Footer and Landind page

---
+++ src/components/App/index.jsx
@@ -0,0 +1,17 @@
+import '../../App.css'
+import Footer from '../Footer/index.jsx'
+import Header from '../Header/index.jsx'
+import Landing from '../Landing/index.jsx'
+
+function App() {
+
+  return (
+      <div>
+        <Header />
+        <Landing />
+        <Footer />
+      </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <header className='banner-container'>
+            <h1><a href="/">Marvel Quiz</a></h1>
+        </header>
+    </div>
+  )
+}
+
+export default Header
