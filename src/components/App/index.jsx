43b654c4ade5733012f2008c1a7f0eb338ae7724
M@teo22 <djokodev75@gmail.com>
2024-12-05 01:28:47 +0100

    feat: design error page

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -19,7 +19,7 @@ function App() {
         <Route path='/welcome' element={<Welcome />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
-        <Route path='/error' element={<ErrorPage />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
 
       <Footer />
--- src/components/ErrorPage/index.jsx
+++ src/components/ErrorPage/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import batman from "../../images/batman.png"
 
 const ErrorPage = () => {
   return (
     <div className='quiz-bg'>
         <div className="container">
-            ErrorPage
+            <h2>Oups, cette page n'hesiste pas !</h2>
+            <img src={batman} alt="error page" />
         </div>
     </div>
   )
